e293f0c3c3e147beccd3c972002d98d380f088ed
rcnchris <rcn.chris@gmail.com>
2025-08-10 14:21:53 +0200

    Création entité Tablette

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Tablette;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+/**
+ * @codeCoverageIgnore
+ */
+final class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadTablettes($manager);
+    }
+
+    private function loadTablettes(ObjectManager $manager): self
+    {
+        $tablette = (new Tablette())
+            ->setName('Catégories des articles')
+            ->setDescription('Liste des catégories d\'articles')
+            ->setIcon('category')
+            ->addChild((new Tablette())->setName('Mémo'))
+            ->addChild((new Tablette())->setName('Présentation'));
+        $manager->persist($tablette);
+
+        $tablette = (new Tablette())
+            ->setName('Catégories des favoris')
+            ->setDescription('Liste des catégories de favoris')
+            ->setIcon('category')
+            ->addChild((new Tablette())->setName('Site officiel'))
+            ->addChild((new Tablette())->setName('Documentation'))
+            ->addChild((new Tablette())->setName('API'));
+        $manager->persist($tablette);
 
         $manager->flush();
+        return $this;
     }
 }
---
+++ src/Entity/Tablette.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Trait\{DateFieldTrait, IdFieldTrait};
+use App\Repository\TabletteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Sluggable\Handler\TreeSlugHandler;
+
+#[ORM\Entity(repositoryClass: TabletteRepository::class)]
+#[Gedmo\Tree(type: 'nested')]
+class Tablette
+{
+    use IdFieldTrait, DateFieldTrait;
+
+    public const ICON = 'hierarchy';
+
+    #[ORM\Column(type: Types::STRING, length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::STRING, length: 255, unique: true)]
+    #[Gedmo\Slug(fields: ['name'])]
+    #[Gedmo\SlugHandler(class: TreeSlugHandler::class, options: [
+        'parentRelationField' => 'parent',
+        'separator' => '-',
+    ])]
+    private ?string $slug = null;
+
+    #[ORM\Column(type: Types::STRING, length: 50, nullable: true)]
+    private ?string $icon = null;
+
+    #[ORM\Column(type: Types::STRING, length: 7)]
+    private string $color = '#000000';
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[Gedmo\TreeLeft]
+    #[ORM\Column(name: 'lft', type: Types::INTEGER)]
+    private ?int $lft = null;
+
+    #[Gedmo\TreeRight]
+    #[ORM\Column(name: 'rgt', type: Types::INTEGER)]
+    private ?int $rgt = null;
+
+    #[Gedmo\TreeLevel]
+    #[ORM\Column(name: 'lvl', type: Types::INTEGER)]
+    private ?int $lvl = null;
+
+    #[Gedmo\TreeRoot]
+    #[ORM\ManyToOne(targetEntity: Tablette::class)]
+    #[ORM\JoinColumn(name: 'tree_root', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    private $root;
+
+    #[Gedmo\TreeParent]
+    #[ORM\ManyToOne(targetEntity: Tablette::class, inversedBy: 'children', cascade: ['persist'])]
+    #[ORM\JoinColumn(name: 'parent_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    private $parent;
+
+    #[ORM\OneToMany(targetEntity: Tablette::class, mappedBy: 'parent', cascade: ['persist'])]
+    #[ORM\OrderBy(['name' => 'ASC'])]
+    private $children;
+
+    public function __construct()
+    {
+        $this->children = new ArrayCollection();
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function getIcon(): ?string
+    {
+        return $this->icon;
+    }
+
+    public function setIcon(?string $icon): static
+    {
+        $this->icon = $icon;
+        return $this;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): static
+    {
+        $this->color = $color;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getLft(): ?int
+    {
+        return $this->lft;
+    }
+
+    public function setLft(int $lft): static
+    {
+        $this->lft = $lft;
+
+        return $this;
+    }
+
+    public function getRgt(): ?int
+    {
+        return $this->rgt;
+    }
+
+    public function setRgt(int $rgt): static
+    {
+        $this->rgt = $rgt;
+
+        return $this;
+    }
+
+    public function getLvl(): ?int
+    {
+        return $this->lvl;
+    }
+
+    public function setLvl(int $lvl): static
+    {
+        $this->lvl = $lvl;
+
+        return $this;
+    }
+
+    public function getRoot(): ?self
+    {
+        return $this->root;
+    }
+
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): self
+    {
+        $this->parent = $parent;
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getChildren(): Collection
+    {
+        return $this->children;
+    }
+
+    public function addChild(self $child): self
+    {
+        if (!$this->children->contains($child)) {
+            $this->children[] = $child;
+            $child->setParent($this);
+        }
+        return $this;
+    }
+
+    public function removeChild(self $child): self
+    {
+        if ($this->children->removeElement($child)) {
+            if ($child->getParent() === $this) {
+                $child->setParent(null);
+            }
+        }
+        return $this;
+    }
+}
